278c9fb29c25a4d31a699b4755a0cfec8d43c608
KexinAnswer <925433202@qq.com>
2019-06-23 22:40:25 +0800

    tcp_class

---
+++ tcp_class/dict_server.cc
@@ -0,0 +1,41 @@
+#include <unordered_map>
+#include "tcp_server.hpp"
+
+// void Handler(std::string& req, std::string* resp) {
+//   auto it = dict.find(req);
+//   if(it == dict.end()) {
+//     *resp = "未找到";
+//   } else {
+//     *resp = it->second;
+//   }
+
+
+
+int main() {
+  std::unordered_map<std::string,std::string> dict;
+
+  dict.insert(std::make_pair<std::string,std::string> ("hello", "你好"));
+  dict.insert(std::make_pair<std::string,std::string> ("world", "世界"));
+  dict.insert(std::make_pair<std::string,std::string> ("bit", "比特"));
+  dict.insert(std::make_pair<std::string,std::string> ("byte", "字节"));
+  TcpServer server;
+  server.Start("0.0.0.0", 9090, [&dict](const std::string& req, std::string *resp){
+    auto it = dict.find(req);
+    if(it == dict.end()) {
+      *resp = "未找到";
+    } else {
+      *resp = it->second;
+    } 
+  });
+
+  return 0;
+}
+
+//[&dict](std::string& req, std::string*resp){
+//auto it = dict.find(req);
+//if(it == dict.end()) {
+//*resp = "未找到";
+//} else {
+//*resp = it->second;
+//} 
+
